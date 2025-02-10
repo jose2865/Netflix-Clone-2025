cb64ed29ee9adbf0f0e7b553c6fc0599d111c270
jose2865 <debhi92@yahoo.com>
2025-02-10 15:40:03 -0600

    axios and request

---
+++ src/utils/axios.js
@@ -0,0 +1,8 @@
+
+import axios from 'axios'; 
+
+const instance = axios.create({
+  baseURL: "https://api.themoviedb.org/3", //base url in Axios for all movies, the starting point for all the movies.
+});
+
+export default instance;
